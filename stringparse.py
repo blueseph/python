82b412ee73f16ae4bb306ebf40cc4f31618f5ef3
Iam Blueseph <blueseph@gmail.com>
2012-04-05 01:35:32 -0400

    i have an idea

--- stringparse.py
+++ stringparse.py
@@ -5,9 +5,9 @@ def StringParse(dodge, crit, cunatk, defender, attacker, death, dmg):
     if defender == 'player':
         initialString = ('The %s attacks.' % attacker)
         if crit == True:
-            initialString = initialString + ('It hits critically!')
+            initialString = initialString + (' It hits critically!')
         if cunatk == True:
-            initialString = initialString + ('The attack finds a weakspot!')
+            initialString = initialString + (' The attack finds a weakspot!')
         initialString = initialString + (' The %s did %s damage.' %s (attacker, dmg))
         if death == True and dodge == False:
             deathString = ('The damage is fatal!')
@@ -15,12 +15,12 @@ def StringParse(dodge, crit, cunatk, defender, attacker, death, dmg):
         elif dodge == True:
             initialString = ('The %s attacks. You dodge!' % attacker)
     else:
-        initialString = ('You attack.')
+        initialString = ('You attack')
         if crit == True:
-            initialString = initialString + (' You hit critically!')
+            initialString = initialString + (' critically!')
         if cunatk == True:
-            initialString = initialString + (' The attack finds a weakspot!')
-        initialString = initialString + (' You did %s damage.' % dmg)
+            initialString = initialString + ('. You find a weakspot!')
+        initialString = initialString + ('. You did %s damage.' % dmg)
         if death == True and dodge == False:
             deathString = ('The damage is fatal!')
             deathStringTwo = ('You have slain the %s!' % defender)
