1d29e2b30f73a0b1535a6a2e750efe9e493b9bee
Iam Blueseph <blueseph@gmail.com>
2012-04-05 22:41:59 -0400

    added class type

--- classes.py
+++ classes.py
@@ -1,14 +1,14 @@
 class Attributes:
-    def __init__(self, str, con, dex, wis, int, cun):
-        self.str = str
-        self.con = con
-        self.dex = dex
-        self.wis = wis
-        self.int = cun
-        self.cun = int
+    def __init__(self, type, str, con, dex, wis, int, cun):
+        self.type = type
+        self.str  = str
+        self.con  = con
+        self.dex  = dex
+        self.wis  = wis
+        self.int  = cun
+        self.cun  = int
 
-Berserker = Attributes(16, 11, 5, 2, 4, 3)
-Warrior = Attributes(6, 18, 7, 5, 4, 5)
-Assassin = Attributes(4, 5, 8, 4, 5, 12)
-Rogue = Attributes(4, 6, 17, 4, 5, 11)
-Orc = Attributes(3, 3, 3, 3, 3, 3) 
+Berserker = Attributes('berserker', 16, 11, 5, 2, 4, 3)
+Warrior = Attributes('warrior', 6, 18, 7, 5, 4, 5)
+Rogue = Attributes('rogue', 4, 6, 17, 4, 5, 11)
+Orc = Attributes('orc', 3, 3, 3, 3, 3, 3) 
