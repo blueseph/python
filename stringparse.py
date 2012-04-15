91c8b2752f0fe251e61636948c2ef4837eb04f84
Iam Blueseph <blueseph@gmail.com>
2012-04-15 19:11:40 -0400

    cleaned up interaction with new melee combat system

--- stringparse.py
+++ stringparse.py
@@ -1,31 +1,10 @@
 import time
 
-def StringParse(dodge, crit, cunatk, defender, attacker, death, dmg, offhand, block):
+def meleeStringParse(dodge, crit, cunatk, defender, attacker, death, dmg, block):
     hasCrit = crit[0]
     deathString = ''
     unitHasDied = False
-    if defender is 'player':
-        if hasCrit is True or cunatk is True:
-            initialString = ('The %s strikes' % attacker)
-        else:
-            initialString = ('The %s attacks.' % attacker)
-        if hasCrit is True:
-            initialString = initialString + (' critically!')
-        if cunatk is True and hasCrit is True:
-            initialString = initialString + (' It finds a weakspot!')
-        elif cunatk is True and hasCrit is False:
-            initialString = initialString + ('. It find a weakspot!')
-        if block is True:
-            initialString = initialString + (' You block some damage!')
-        initialString = initialString + (' The %s did %s damage.' % (attacker, dmg))
-        if death is True and dodge is False:
-            deathString = ('The damage is fatal!')
-            deathStringTwo = ('You have been slain.')
-        if dmg <= 0:
-            initialString = ('The %s attacks. The attack does no damage.' % attacker)
-        elif dodge is True:
-            initialString = ('The %s attacks. You dodge!' % attacker)
-    else:
+    if attacker.type is 'berserker' or attacker.type is 'rogue' or attacker.type is'warrior':
         if hasCrit is True or cunatk is True:
             initialString = ('You attack')
         else:
@@ -39,30 +18,54 @@ def StringParse(dodge, crit, cunatk, defender, attacker, death, dmg, offhand, bl
         if block is True:
             initialString = initialString + (' It blocks some damage!')
         initialString = initialString + (' You did %s damage.' % dmg)
-        if death is True and dodge is False:
+        if death[0] is True and dodge is False:
             deathString = ('The damage is fatal!')
-            deathStringTwo = ('You have slain the %s!' % defender)
+            deathStringTwo = ('You have slain the %s!' % defender.type)
         if dmg <= 0:
             initialString = ('You attack. The attack does no damage.')
         if dodge is True:
-            initialString = ('You attack. The %s dodges!' % defender)
+            initialString = ('You attack. The %s dodges!' % defender.type)
+    elif attacker.type is not 'berserker' or 'rogue' or 'warrior':
+        if hasCrit is True or cunatk is True:
+            initialString = ('The %s strikes' % attacker.type)
+        else:
+            initialString = ('The %s attacks.' % attacker.type)
+        if hasCrit is True:
+            initialString = initialString + (' critically!')
+        if cunatk is True and hasCrit is True:
+            initialString = initialString + (' It finds a weakspot!')
+        elif cunatk is True and hasCrit is False:
+            initialString = initialString + ('. It find a weakspot!')
+        if block is True:
+            initialString = initialString + (' You block some damage!')
+        initialString = initialString + (' The %s did %s damage.' % (attacker.type, dmg))
+        if death[0] is True and dodge is False:
+            deathString = ('The damage is fatal!')
+            deathStringTwo = ('You have been slain.')
+        if dmg <= 0:
+            initialString = ('The %s attacks. The attack does no damage.' % attacker.type)
+        elif dodge is True:
+            initialString = ('The %s attacks. You dodge!' % attacker.type)
 
-    if 'The' in deathString:
-        unitHasDied = True
+    if death[0] is True:
         print(initialString)
-        time.sleep(1)
+        time.sleep(1.5)
         print(deathString)
-        time.sleep(2)
+        time.sleep(1.5)
         print(deathStringTwo)
-        time.sleep(1)
+        time.sleep(2.5)
     else:
         print(initialString)
         time.sleep(1)
-    return unitHasDied
 
-def dispHP(playerhp, playermaxhp, orchp, orcmaxhp, sleeptime):
-    time.sleep(sleeptime)
+def dispHP(playerhp, playermaxhp, orchp, orcmaxhp):
+    if playerhp < 0:
+        playerhp = 0
+    if orchp < 0:
+        orchp = 0
     print(' ')
+    time.sleep(1)
     print('         You: (%s/%s HP)       Him: (%s/%s HP)' % (playerhp, playermaxhp, orchp, orcmaxhp) )
+    time.sleep(1.5)
     print(' ')
 
