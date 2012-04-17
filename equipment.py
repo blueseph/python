a218c75b7fe333be15092b503dd68a4ea524d89c
Iam Blueseph <blueseph@gmail.com>
2012-04-17 05:25:03 -0400

    moved everything to do with equipment to equipment.py. added staff

--- equipment.py
+++ equipment.py
@@ -1,3 +1,6 @@
+import itemlist
+import random
+
 class Item:
     def __init__(self, name, weight, goldValue):
         self.name       = name
@@ -41,8 +44,36 @@ class Armor(Equippable):
          Equippable.__init__(self, name, weight, type, goldValue)
          self.armorClass = armorClass
         
-        
-        
+def chooseWeapon(weapon_list, weaponChoice): #getting weapon properties out of the itemlist
+    name         = {}
+    name         = itemlist.weapon_list[weaponChoice][0]
+    goldValue    = itemlist.weapon_list[weaponChoice][1]
+    wepMin       = itemlist.weapon_list[weaponChoice][2]
+    wepMax       = itemlist.weapon_list[weaponChoice][3]
+    wepCrit      = itemlist.weapon_list[weaponChoice][4]
+    wepCritDmg   = itemlist.weapon_list[weaponChoice][5]
+    wepRange     = itemlist.weapon_list[weaponChoice][6]
+    weight       = itemlist.weapon_list[weaponChoice][7]
+    type         = itemlist.weapon_list[weaponChoice][8]
+    return name, weight, type, goldValue, wepMin, wepMax, wepCrit, wepCritDmg, wepRange 
+
+def chooseArmor(armor_list, armorChoice): #getting armor properties out of the itemlist
+    name        = {}
+    name        = itemlist.armor_list[armorChoice][0]
+    goldValue   = itemlist.armor_list[armorChoice][1]
+    armorClass  = itemlist.armor_list[armorChoice][2]
+    weight      = itemlist.armor_list[armorChoice][3]
+    type        = itemlist.armor_list[armorChoice][4]
+    return name, weight, type, goldValue, armorClass #armor type currently not implimented
+
+def chooseShield(shield_list, shieldChoice): #getting shield properties out of the itemlist
+    name = {}
+    name        = itemlist.shield_list[shieldChoice][0]
+    goldValue   = itemlist.shield_list[shieldChoice][1]
+    blockValue  = itemlist.shield_list[shieldChoice][2]
+    weight      = itemlist.shield_list[shieldChoice][3]
+    type        = itemlist.shield_list[shieldChoice][4]
+    return name, weight, type, goldValue, blockValue        
         
          
          
