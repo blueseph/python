117c2818d26c19c1ecdcc0d81ce8e95c5a0bafc6
Iam Blueseph <blueseph@gmail.com>
2012-04-17 05:23:50 -0400

    moved things to their logical file. cleaned up death calculation

--- combat.py
+++ combat.py
@@ -1,161 +1,33 @@
 import random
 import time
-import itemlist
 import classes
 import stringparse
-import equipment
-import abilities
+import spells
 
-class Unit:
-    def __init__(self, type):
-        self.type           = type.type
-        self.str            = type.str
-        self.con            = type.con
-        self.dex            = type.dex
-        self.wis            = type.wis
-        self.int            = type.int
-        self.cun            = type.cun
-        self.maxhp          = self.con * 14
-        self.curhp          = self.maxhp
-        if self.dex > self.str:
-            self.dmg        = self.dex//2.6
-        else:
-            self.dmg        = self.str//1.3
-        self.mainhand       = None
-        self.offhand        = None
-        self.offhandWeight  = 0
-        self.armorWeight    = 0
-
-    def equip(self, item):
-        if (item.type is '1h' or '2h') and (self.mainhand is None): #deletes currently equipped items. need to fix
-            self.mainhand           = item.type
-            self.mainhandWeaponName = item.name
-            self.mainhandWeight     = item.weight
-            self.mainhandGoldValue  = item.goldValue
-            self.mainhandWepMin     = item.wepMin
-            self.mainhandWepMax     = item.wepMax
-            self.mainhandCrit       = item.wepCrit
-            self.mainhandCritDmg    = item.wepCritDmg
-            self.mainhandRange      = item.wepRange # need to impliment range
-        elif self.mainhand is '1h' and item.type is '1h':
-            self.offhand            = item.type
-            self.offhandItemName    = item.name
-            self.offhandWeight      = item.weight
-            self.offhandGoldValue   = item.goldValue
-            self.offhandWepMin      = item.wepMin
-            self.offhandWepMax      = item.wepMax
-            self.offhandCrit        = item.wepCrit
-            self.offhandCritDmg     = item.wepCritDmg
-            self.offhandRange       = item.wepRange
-        elif item.type is 'shield':
-            self.offhand            = item.type
-            self.offhandItemName    = item.name
-            self.offhandWeight      = item.weight
-            self.offhandGoldValue   = item.goldValue
-            self.offhandBlockValue  = item.blockValue
-            self.offhandBlockChance = item.blockChance
-        elif (item.type is not 'shield' or '1h' or '2h'):
-            self.armorName          = item.name
-            self.armorWeight        = item.weight
-            self.armorGoldValue     = item.goldValue
-            self.armorClass         = item.armorClass
-#        elif (item.type is 'shield' and self.offhand is not None) or (item.type is '1h' and self.offhand is not None)
-        self.weight = self.mainhandWeight + self.offhandWeight + self.armorWeight
-            
-def chooseWeapon(weapon_list, weaponChoice): #getting weapon properties out of the itemlist
-    name         = {}
-    name         = itemlist.weapon_list[weaponChoice][0]
-    goldValue    = itemlist.weapon_list[weaponChoice][1]
-    wepMin       = itemlist.weapon_list[weaponChoice][2]
-    wepMax       = itemlist.weapon_list[weaponChoice][3]
-    wepCrit      = itemlist.weapon_list[weaponChoice][4]
-    wepCritDmg   = itemlist.weapon_list[weaponChoice][5]
-    wepRange     = itemlist.weapon_list[weaponChoice][6]
-    weight       = itemlist.weapon_list[weaponChoice][7]
-    type         = itemlist.weapon_list[weaponChoice][8]
-    return name, weight, type, goldValue, wepMin, wepMax, wepCrit, wepCritDmg, wepRange 
-
-def chooseArmor(armor_list, armorChoice): #getting armor properties out of the itemlist
-    name        = {}
-    name        = itemlist.armor_list[armorChoice][0]
-    goldValue   = itemlist.armor_list[armorChoice][1]
-    armorClass  = itemlist.armor_list[armorChoice][2]
-    weight      = itemlist.armor_list[armorChoice][3]
-    type        = itemlist.armor_list[armorChoice][4]
-    return name, weight, type, goldValue, armorClass #armor type currently not implimented
-
-def chooseShield(shield_list, shieldChoice): #getting shield properties out of the itemlist
-    name = {}
-    name        = itemlist.shield_list[shieldChoice][0]
-    goldValue   = itemlist.shield_list[shieldChoice][1]
-    blockValue  = itemlist.shield_list[shieldChoice][2]
-    weight      = itemlist.shield_list[shieldChoice][3]
-    type        = itemlist.shield_list[shieldChoice][4]
-    return name, weight, type, goldValue, blockValue
-
-def chooseClass():
-    print('Choose your class')
-    print(' ')
-    print('''B: Berserker (STR, CON)
-R: Rogue (DEX, CUN)
-W: Warrior (CON, STR)
-''')
-    playerClass = input()
-    if playerClass.lower().startswith('w'):
-        playerClass = classes.Warrior
-        weapon = equipment.Weapon(*chooseWeapon(itemlist.weapon_list, 0))
-        armor  = equipment.Armor(*chooseArmor(itemlist.armor_list, 4))
-        offhand  = equipment.Shield(*chooseShield(itemlist.shield_list, 1))
-        hasOffhand = True
-    elif playerClass.lower().startswith('b'):
-        playerClass = classes.Berserker
-        weapon = equipment.Weapon(*chooseWeapon(itemlist.weapon_list, 5))
-        armor  = equipment.Armor(*chooseArmor(itemlist.armor_list, 2))
-        hasOffhand = False
-    elif playerClass.lower().startswith('r'):
-        playerClass = classes.Rogue
-        weapon = equipment.Weapon(*chooseWeapon(itemlist.weapon_list, 3))
-        offhand = equipment.Weapon(*chooseWeapon(itemlist.weapon_list, 3))
-        armor  = equipment.Armor(*chooseArmor(itemlist.armor_list, 1))
-        hasOffhand = True
-    global player
-    player = Unit(playerClass)
-    Unit.equip(player, weapon) 
-    Unit.equip(player, armor)
-    if hasOffhand is True:
-        Unit.equip(player, offhand)
-
-def chooseMonsterClass():
-    monsterClass = classes.Orc
-    global monster
-    monster = Unit(monsterClass)
-    weapon = equipment.Weapon(*chooseWeapon(itemlist.weapon_list, random.randint(0, 7)))
-    armor  = equipment.Armor(*chooseArmor(itemlist.armor_list, random.randint(0, 5)))
-    Unit.equip(monster, weapon)
-    Unit.equip(monster, armor)
-    
-def decideTurn(playerWeight, monsterWeight): 
+def decideTurn(player, monster):
+    playerWeight = player.weight
+    monsterWeight = monster.weight
     if (playerWeight - monsterWeight) > 0:
-        initiator = monster
-        defender = player
+        initiator = classes.monster
+        defender = classes.player
         if playerWeight//monsterWeight < 1:
             tpr = 1
         elif playerWeight//monsterWeight > 3:
             tpr = 3
         else:
             tpr = playerWeight//monsterWeight
-    elif (playerWeight - monsterWeight) <0:
-        initiator = player
-        defender = monster
-        if (playerWeight//playerWeight * -1) < 1:
+    elif (playerWeight - monsterWeight) < 0:
+        initiator = classes.player
+        defender = classes.monster
+        if (monsterWeight//playerWeight * -1) < 1:
             tpr = 1
-        elif (playerWeight//playerWeight * -1) > 3:
+        elif (monsterWeight//playerWeight * -1) > 3:
             tpr = 3
         else:
-            tpr = playerWeight//playerWeight * -1
+            tpr = monsterWeight//playerWeight * -1
     else:
-        initiator = player
-        defender = monster
+        initiator = classes.player
+        defender = classes.monster
         tpr = 1
     return tpr, initiator, defender
 
@@ -182,18 +54,23 @@ def meleeAttack(attacker, defender):
             wepOhDmg *= attacker.offhandCrit
     if wepOhDmg < 0:
         wepOhDmg = 0
-    wepTotDmg = int(wepMhDmg) + int(wepOhDmg)
+    wepTotDmg = int(wepMhDmg) + int(wepOhDmg/2)
     if wepTotDmg < 0:
         wepTotDmg = 0
     if unitHasDodged == True:
         wepTotDmg = 0
     defender.curhp -= wepTotDmg
+    unitDeath = determineDeath(defender)
+    stringparse.meleeStringParse(unitHasDodged, unitHasCrit, unitCunAtk, defender, attacker, unitDeath, wepTotDmg, unitHasBlocked)
+    return unitDeath
+
+def determineDeath(defender):
+    unitDeath = (False, 'nobody')
     if defender.curhp <= 0:
-        if defender.type is not 'berserker' and defender.type is not 'rogue' and defender.type is not 'warrior':
+        if defender.type is 'monster':
             unitDeath = (True, 'monster')
         else:
             unitDeath = (True, 'player')
-    stringparse.meleeStringParse(unitHasDodged, unitHasCrit, unitCunAtk, defender, attacker, unitDeath, wepTotDmg, unitHasBlocked)
     return unitDeath
 
 def getDmgRoll(unit):
@@ -239,56 +116,43 @@ def cunningAttack(unit):
 def gameTurn(tpr, attacker, defender):
     if defender.curhp > 0 and attacker.curhp > 0:
         for i in range(tpr):
-            unitDeath = meleeAttack(attacker, defender)
+            if attacker.unitclass is 'wizard':
+                unitDeath = spells.castSpell(attacker, defender, 0)
+            else:
+                unitDeath = meleeAttack(attacker, defender)
             if unitDeath[0] is True:
                 break
-        return unitDeath
+    return unitDeath
             
-def fight(initiator):
+def fight():
     print(' ')
     print('An enemy approaches!')
     print(' ')
     time.sleep(1)
     print('Equipment:')
     time.sleep(1)
-    if player.offhand is not None:
-        print('You: %s (mainhand), %s (offhand), %s (%s ac)' % (player.mainhandWeaponName, player.offhandItemName, player.armorName, player.armorClass))
-    elif player.mainhand is '2h':
-        print('You: %s (two-handed), %s (%s ac)' % (player.mainhandWeaponName, player.armorName, player.armorClass))
+    if classes.player.offhand is not None:
+        print('You: %s (mainhand), %s (offhand), %s (%s ac)' % (classes.player.mainhandWeaponName, classes.player.offhandItemName, classes.player.armorName, classes.player.armorClass))
+    elif classes.player.mainhand is '2h':
+        print('You: %s (two-handed), %s (%s ac)' % (classes.player.mainhandWeaponName, classes.player.armorName, classes.player.armorClass))
     else:
-        print('You: %s (mainhand), %s (%s ac)' % (player.mainhandWeaponName, player.armorName, player.armorClass))
+        print('You: %s (mainhand), %s (%s ac)' % (classes.player.mainhandWeaponName, classes.player.armorName, classes.player.armorClass))
     time.sleep(.5)
-    if monster.offhand is not None:
-        print('%s: %s (mainhand), %s (offhand), %s (%s ac)' % (monster.type, monster.mainhandWeaponName, monster.offhandItemName, monster.armorName, monster.armorClass))
-    elif monster.mainhand is '2h':
-        print('%s: %s (two-handed), %s (%s ac)' % (monster.type, monster.mainhandWeaponName, monster.armorName, monster.armorClass))
+    if classes.monster.offhand is not None:
+        print('%s: %s (mainhand), %s (offhand), %s (%s ac)' % (classes.monster.clas, classes.monster.mainhandWeaponName, classes.monster.offhandItemName, classes.monster.armorName, classes.monster.armorClass))
+    elif classes.monster.mainhand is '2h':
+        print('%s: %s (two-handed), %s (%s ac)' % (classes.monster.unitclass, classes.monster.mainhandWeaponName, classes.monster.armorName, classes.monster.armorClass))
     else:
-        print('%s: %s (mainhand), %s (%s ac)' % (monster.type, monster.mainhandWeaponName, monster.armorName, monster.armorClass))
-    stringparse.dispHP(player.curhp, player.maxhp, monster.curhp, monster.maxhp)
+        print('%s: %s (mainhand), %s (%s ac)' % (classes.monster.unitclass, classes.monster.mainhandWeaponName, classes.monster.armorName, classes.monster.armorClass))
+    stringparse.dispHP(classes.player.curhp, classes.player.maxhp, classes.monster.curhp, classes.monster.maxhp)
     time.sleep(1)
-    while player.curhp > 0 and monster.curhp > 0:
+    tpr, initiator, defender =  decideTurn(classes.player, classes.monster)
+    while classes.player.curhp > 0 and classes.monster.curhp > 0:
         unitDeath = gameTurn(tpr, initiator, defender)
         if unitDeath[0] is True:
-            stringparse.dispHP(player.curhp, player.maxhp, monster.curhp, monster.maxhp)
+            stringparse.dispHP(classes.player.curhp, classes.player.maxhp, classes.monster.curhp, classes.monster.maxhp)
             break
+        time.sleep(.5)
         unitDeath = gameTurn(1, defender, initiator)
-        stringparse.dispHP(player.curhp, player.maxhp, monster.curhp, monster.maxhp)
+        stringparse.dispHP(classes.player.curhp, classes.player.maxhp, classes.monster.curhp, classes.monster.maxhp)
     return unitDeath
-
-chooseClass()                                                       #chooses player class
-chooseMonsterClass()                                                #chooses monster class
-tpr, initiator, defender = decideTurn(player.weight, monster.weight)   #determines who goes first and how many turns per round
-kill_count = 0
-unitDeath = fight(initiator)          #initiates fight
-
-while 'player' not in unitDeath:
-        kill_count += 1
-        chooseMonsterClass()
-        tpr, initiator, defender = decideTurn(player.weight, monster.weight)
-        unitDeath = fight(initiator) #rerolls monster and fights again
-
-if kill_count is 1:
-	print('You have died. You killed %s monster.' % kill_count)
-else:
-	print('You have died. You killed %s monsters.' % kill_count)
-time.sleep(10)
