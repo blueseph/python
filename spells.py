9ec70b89e869d1882b260994cc5d3c050fc3f363
Iam Blueseph <blueseph@gmail.com>
2012-04-24 21:11:24 -0400

    added new display function

--- spells.py
+++ spells.py
@@ -3,11 +3,12 @@ import combat
 import stringparse
 
 spells = {
-    #   name                    description       type    base   int scaling
-    #   0                        1                 2       4      5
+    #   name                    description                 type    base scaling  cd
+    #   0                        1                           2       3     4       5
     'wizard': [
-    ['fireball', 'casts a fireball at an enemy',  'dmg', (10,15), .6],
-    [    'heal',             'heals the player', 'heal',   (6-8), .2],
+    [       'fireball',  'casts a fireball at an enemy',  'dmg', (20,25), .7,      5],
+    [  'magic missile',         'casts a magic missile',  'dmg', (10,15), .3,      0],
+    [           'heal',              'heals the player', 'heal',   (6-8), .2,     20],
     ]
     }
 
@@ -17,6 +18,7 @@ def castSpell(caster, defender, spellChoice):
     spellBaseMin    = spells[caster.unitclass][spellChoice][3][0]
     spellBaseMax    = spells[caster.unitclass][spellChoice][3][1]
     spellScale      = spells[caster.unitclass][spellChoice][4]
+    spellCooldown   = spells[caster.unitclass][spellChoice][5]
     spellMag = int(random.randint(spellBaseMin, spellBaseMax) + (caster.int * spellScale))
     spellInfo = spellName, spellType, spellBaseMin, spellBaseMax, spellScale, spellMag
     if spellType is 'dmg':              #i want this to be automated
@@ -24,5 +26,5 @@ def castSpell(caster, defender, spellChoice):
     if spellType is 'heal':
         attacker.curhp += spellMag
     unitDeath = combat.determineDeath(defender)
-    stringparse.castStringParse(caster, defender, spellInfo, unitDeath)
-    return unitDeath
+    return unitDeath, spellInfo
+    
