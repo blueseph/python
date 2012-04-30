90106dfc54db4582d9e65907fc0004947d1a56fc
Iam Blueseph <blueseph@gmail.com>
2012-04-29 20:18:38 -0400

    added leveling system. specified player and nonplayer

--- classes.py
+++ classes.py
@@ -1,9 +1,11 @@
 import equipment
 import itemlist
 import random
+import levelingstats
+import gameturn
 
 class Attributes:
-    def __init__(self, unitclass, str, con, dex, wis, int, cun):
+    def __init__(self, unitclass, str, con, dex, wis, int, cun, curlvl):
         self.unitclass  = unitclass
         self.str        = str
         self.con        = con
@@ -11,34 +13,42 @@ class Attributes:
         self.wis        = wis
         self.int        = int
         self.cun        = cun
+        self. curlvl    = curlvl
 
-class Unit:
-    def __init__(self, type):
-        self.unitclass           = type.unitclass
-        if self.unitclass is not 'berserker' and self.unitclass is not 'rogue' and self.unitclass is not 'warrior' and self.unitclass is not 'wizard':
-            self.type       = 'monster'
-        else:
-            self.type       = 'player'
-        self.str            = type.str
-        self.con            = type.con
-        self.dex            = type.dex
-        self.wis            = type.wis
-        self.int            = type.int
-        self.cun            = type.cun
-        self.maxhp          = self.con * 14
-        self.curhp          = self.maxhp
+class Creature(Attributes):
+    
+    ###############################
+    #       sets initial stats    #
+    ###############################
+
+    def calculateStats(self):
+        self.maxhp      = self.con * 14
         if self.dex > self.str:
-            self.dmg        = self.dex//2.6
+            self.dmg    = int(self.dex//2.6)
         else:
-            self.dmg        = self.str//1.3
+            self.dmg    = int(self.str//1.3)
+    
+    def __init__(self, Attributes):
+        self.unitclass      = Attributes.unitclass
+        self.str            = Attributes.str
+        self.con            = Attributes.con
+        self.dex            = Attributes.dex
+        self.wis            = Attributes.wis
+        self.int            = Attributes.int
+        self.cun            = Attributes.cun
+        self.curlvl         = Attributes.curlvl
+        self.type           = 'monster'
+        self.calculateStats()
+        self.curhp          = self.maxhp
         self.mainhand       = None
         self.offhand        = None
         self.offhandWeight  = 0
         self.armorWeight    = 0
         self.inventory      = []
 
-    def addInventoryItem(self, item):
-        self.inventory.append(item)
+    ###############################
+    #       equipment system      #
+    ###############################
 
     def equip(self, item):
         if (item.type is '1h' or '2h') and (self.mainhand is None): #deletes currently equipped items. need to fix
@@ -73,8 +83,53 @@ class Unit:
             self.armorWeight        = item.weight
             self.armorGoldValue     = item.goldValue
             self.armorClass         = item.armorClass
-#        elif (item.type is 'shield' and self.offhand is not None) or (item.type is '1h' and self.offhand is not None)
         self.weight = self.mainhandWeight + self.offhandWeight + self.armorWeight
+
+    ###############################
+    #       inventory system      #
+    ###############################
+
+    def addInventoryItem(self, item): 
+        self.inventory.append(item)
+
+    ###############################
+    #          xp system          #
+    ###############################
+    
+    def giveXP(self):
+        return int(10 + (self.curlvl ** 3.05))
+        
+class Player(Creature):            
+    def __init__(self, Creature):
+        self.unitclass      = Creature.unitclass
+        self.str            = Creature.str
+        self.type           = 'player'
+        self.con            = Creature.con
+        self.dex            = Creature.dex
+        self.wis            = Creature.wis
+        self.int            = Creature.int
+        self.cun            = Creature.cun
+        self.curlvl         = Creature.curlvl
+        if self.curlvl is 1:
+            self.curXP      = 0
+        else:
+            self.curXP      = levelingstats.xpToLevel[self.curLvl - 1]
+        self.calculateStats()
+        self.curhp          = self.maxhp
+        self.mainhand       = None
+        self.offhand        = None
+        self.offhandWeight  = 0
+        self.armorWeight    = 0
+        self.inventory      = []
+
+    ###############################
+    #     player xp system        #
+    ###############################
+    
+    def gainXP(self, xp):
+        self.curXP += xp
+        if self.curXP > levelingstats.xpToLevel[self.curlvl + 1]:
+            gameturn.unitGainLvl = True
 
 def chooseClass():
     print('Choose your class')
@@ -108,30 +163,30 @@ w: Wizard (INT, WIS)
         armor  = equipment.Armor(*equipment.chooseArmor(itemlist.armor_list, 0))
         hasOffhand = False
     global player
-    player = Unit(playerClass)
-    Unit.addInventoryItem(player, weapon)
-    Unit.addInventoryItem(player, armor)
-    Unit.equip(player, weapon) 
-    Unit.equip(player, armor)
+    player = Player(Creature(playerClass))
+    player.addInventoryItem(weapon)
+    player.addInventoryItem(armor)
+    player.equip(weapon) 
+    player.equip(armor)
     if hasOffhand is True:
-        Unit.equip(player, offhand)
-        Unit.addInventoryItem(player, offhand)
+        player.equip(offhand)
+        player.addInventoryItem(offhand)
 
 def chooseMonsterClass():
     monsterClass = Orc
     global monster
-    monster = Unit(monsterClass)
+    monster = Creature(monsterClass)
     weapon = equipment.Weapon(*equipment.chooseWeapon(itemlist.weapon_list, random.randint(0, 7)))
     armor  = equipment.Armor(*equipment.chooseArmor(itemlist.armor_list, random.randint(0, 5)))
-    Unit.addInventoryItem(monster, weapon)
-    Unit.addInventoryItem(monster,armor)
-    Unit.equip(monster, weapon)
-    Unit.equip(monster, armor)
+    monster.addInventoryItem(weapon)
+    monster.addInventoryItem(armor)
+    monster.equip(weapon)
+    monster.equip(armor)
     
 
-#                           str, con, dex, wis, int, cun
-Berserker = Attributes('berserker', 16, 11, 5, 2, 4, 3)
-Warrior = Attributes('warrior', 6, 18, 7, 5, 4, 5)
-Rogue = Attributes('rogue', 4, 6, 17, 4, 5, 11)
-Wizard = Attributes('wizard', 3, 5, 4, 12, 22, 4)
-Orc = Attributes('orc', 3, 3, 3, 3, 3, 3) 
+#                           str, con, dex, wis, int, cun, lvl
+Berserker = Attributes('berserker', 16, 11, 5, 2, 4, 3, 1)
+Warrior = Attributes('warrior', 14, 18, 7, 5, 4, 5, 1)
+Rogue = Attributes('rogue', 4, 6, 17, 4, 5, 11, 1)
+Wizard = Attributes('wizard', 3, 5, 4, 12, 22, 4, 1)
+Orc = Attributes('orc', 3, 3, 3, 3, 3, 3, 1) 
