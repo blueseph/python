0cabc7602d4c33a4563ab62a9145881c6eef8bdb
Iam Blueseph <blueseph@gmail.com>
2012-04-29 20:22:04 -0400

    changed starting flags

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import combat
 import equipment
 import classes
 import time
+import gameturn
 
-classes.chooseClass()                           #chooses player class
-classes.chooseMonsterClass()                    #chooses monster class
-kill_count = 0
-unitDeath = combat.fight()          #initiates fight
+    ###############################
+    #   chooses class and fights  #
+    ###############################
+    
+if __name__ == "__main__":
+        classes.chooseClass()                           
+        classes.chooseMonsterClass()
+        kill_count = 0
+        combat.fight()                                  
 
-while 'player' not in unitDeath:
-    kill_count += 1
-    classes.chooseMonsterClass()        #rerolls monster
-    unitDeath = combat.fight()          #fights again
+        while gameturn.playerDeath is False:
+            kill_count += 1
+            classes.chooseMonsterClass()        #rerolls monster
+            combat.fight()          #fights again
 
-if kill_count is 1:
-	print('You have died. You killed %s monster.' % kill_count)
-else:
-	print('You have died. You killed %s monsters.' % kill_count)
-time.sleep(10)
+        if kill_count is 1:
+                print('You have died. You killed %s monster.' % kill_count)
+        else:
+        	print('You have died. You killed %s monsters.' % kill_count)
+        time.sleep(10)
