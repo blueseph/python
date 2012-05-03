e4403492bb9cfcb38b180ebef41d4f5844eb9ee5
Iam Blueseph <blueseph@gmail.com>
2012-05-03 02:04:20 -0400

    removed kill counter

--- main.py
+++ main.py
@@ -11,16 +11,10 @@ import gameturn
 if __name__ == "__main__":
         classes.chooseClass()                           
         classes.chooseMonsterClass()
-        kill_count = 0
         combat.fight()                                  
 
         while gameturn.playerDeath is False:
-            kill_count += 1
             classes.chooseMonsterClass()        #rerolls monster
             combat.fight()          #fights again
 
-        if kill_count is 1:
-                print('You have died. You killed %s monster.' % kill_count)
-        else:
-        	print('You have died. You killed %s monsters.' % kill_count)
-        time.sleep(10)
+        time.sleep(5)
