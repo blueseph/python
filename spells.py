0ab98e3ee6a54abfce5e47ba74ef6cac3b8b4c8a
Iam Blueseph <blueseph@gmail.com>
2012-05-13 15:53:09 -0400

    added framework for buffs and ability system

--- spells.py
+++ spells.py
@@ -1,90 +1,127 @@
-import random
-import combat
-import stringparse
-from operator import itemgetter
+
+    ###############################
+    #            spells           #
+    ###############################
+
+class Spell():
+    def __init__(self, name, description, type, base, scaling, cd, priority, range):
+        self.name           = name
+        self.description    = description
+        self.type           = type
+        self.base           = base
+        self.scaling        = scaling
+        self.cd             = cd
+        self.priority       = priority
+        self.range          = range     #not implemented
+
+    ###############################
+    #          spell list         #
+    ###############################
 
 spells = {
-    #                          description                 type    base    scaling  cd   prior
+    #                          description                 type    base    scaling  cd  prior  range
     #                            0                           1       2        3      4    5
-    'wizard': {
-           'fireball':  ('casts a fireball at an enemy',  'dmg', (10, 15),   2.5,     6,  9),
-      'magic missile':         ('casts a magic missile',  'dmg', (4,  8),    1.8,     0,  5),
-               'heal':              ('heals the player', 'heal', (10, 15),     2,    12,  0),
-    }
+           'fireball':  ('casts a fireball at an enemy',  'dmg', (10, 15),   2.5,     6,  9,    0),
+      'magic missile':         ('casts a magic missile',  'dmg', (4,  8),    1.3,     0,  5,    0),
+               'heal':              ('heals the player', 'heal', (10, 15),     2,    12,  0,    0),
     }
 
-spellCooldowns = {}        
+    ###############################
+    #       activate spells       #
+    ###############################
 
-def getSpellAvail(spell):
+Fireball        = Spell('fireball', spells['fireball'][0], spells['fireball'][1], spells['fireball'][2], spells['fireball'][3], spells['fireball'][4], spells['fireball'][5], spells['fireball'][6])
+Magic_missile   = Spell('magic missile', spells['magic missile'][0], spells['magic missile'][1], spells['magic missile'][2], spells['magic missile'][3], spells['magic missile'][4], spells['magic missile'][5], spells['magic missile'][6])
+Heal            = Spell('heal', spells['heal'][0], spells['heal'][1], spells['heal'][2], spells['heal'][3], spells['heal'][4], spells['heal'][5], spells['heal'][6])
 
     ###############################
-    # if cd is either not listed  #
-    #  or 0, spell is available   #
+    #            buffs            #
     ###############################
-    
-    try:
-        if spellCooldowns[spell] is not 0:
-            return False
-        else:
-            return True
-    except:
-        return True
 
-def useHeal(caster):
+
+class Buff():
+    def __init__(self, name, description, target, type, affects, duration, magnitude):
+        self.name           = name
+        self.description    = description
+        self.target         = target
+        self.type           = type
+        self.affects        = affects
+        self.duration       = duration
+        self.magnitude      = magnitude
 
     ###############################
-    #   use heal when it is most  #
-    #          effecient          #
+    #        buff list            #
     ###############################
+
+buffs = { 
+    #               name                            description                       target     type                 affects          duration                 magnitude    
+    #                                                   0                                1         2                      3               4                         5
+                 'Bloodlust':                    ('Heals self for small amount',       'self',    'melee',             'curhp',           1,                           10),
+                 'Momentum':              ('Next attack will strike critically',       'self',    'melee',              'crit',           2,           (True, True, True)),
+                 'Backstab':                   ('Next attack will pierce armor',       'self',    'melee',               'ap',            2,                         True),
+                 'Shield Block':       ('Chance to block doubled for next turn',       'self',    'melee',       'blockchance',           2,                            2),
+                 'Overcharge':          ('Increases power of next spell by 40%',       'self',    'spell',          'spellmag',           2,                          1.4),
+
+    }
+ 
+    ###############################
+    #       activate buffs        #
+    ###############################
+
+Buff_bloodlust       = Buff('Bloodlust', buffs['Bloodlust'][0], buffs['Bloodlust'][1], buffs['Bloodlust'][2], buffs['Bloodlust'][3], buffs['Bloodlust'][4], buffs['Bloodlust'][5])
+Buff_momentum        = Buff('Momentum', buffs['Momentum'][0], buffs['Momentum'][1], buffs['Momentum'][2], buffs['Momentum'][3], buffs['Momentum'][4], buffs['Momentum'][5])
+Buff_backstab        = Buff('Backstab', buffs['Backstab'][0], buffs['Backstab'][1], buffs['Backstab'][2], buffs['Backstab'][3], buffs['Backstab'][4], buffs['Backstab'][5])
+Buff_shield_block    = Buff('Shield Block', buffs['Shield Block'][0], buffs['Shield Block'][1], buffs['Shield Block'][2], buffs['Shield Block'][3], buffs['Shield Block'][4], buffs['Shield Block'][5])
+Buff_overcharge      = Buff('Overcharge', buffs['Overcharge'][0], buffs['Overcharge'][1], buffs['Overcharge'][2], buffs['Overcharge'][3], buffs['Overcharge'][4], buffs['Overcharge'][5])
     
-    castHeal = False
-    avgHealAmt = (((spells[caster.unitclass]['heal'][2][0] + spells[caster.unitclass]['heal'][2][1]) / 2) + (caster.int * spells[caster.unitclass]['heal'][3]))
-    if caster.curhp < (caster.maxhp - avgHealAmt):
-        castHeal = getSpellAvail('heal')
-    return castHeal
 
-def getSpellToCast(caster):
-    spellToCast = ''
-    castHeal = useHeal(caster)
-    if castHeal is True:
-        spellToCast = 'heal'
-    if castHeal is False:
-        spellPriority = []
+    ###############################
+    #          abilities          #
+    ###############################
 
-        ###############################
-        # checks spell with highest   #
-        # priority for cd, then casts #
-        ###############################
-    
-        for i in spells[caster.unitclass]:
-            spellPriority.append((i, spells[caster.unitclass][i][5]))
-        spellPriority = sorted(spellPriority, key=itemgetter(1), reverse=True)
-        for i in range(len(spellPriority)):
-            if getSpellAvail(spellPriority[i][0]) is True:
-                spellToCast = spellPriority[i][0]
-                break
-            else:
-                pass
-    return spellToCast
+class Ability():
+    def __init__(self, name, description, occurance, effect, affects, magnitude, buffplaced):
+        self.name           = name
+        self.description    = description
+        self.occurance      = occurance
+        self.effect         = effect
+        self.affects        = affects
+        self.magnitude      = magnitude
+        self.cooldown       = cooldown
+        self.buffplaced     = buffplaced
 
-def castSpell(caster, defender, spellChoice):
-    spellName       = spellChoice
-    spellType       = spells[caster.unitclass][spellChoice][1]
-    spellBaseMin    = spells[caster.unitclass][spellChoice][2][0]
-    spellBaseMax    = spells[caster.unitclass][spellChoice][2][1]
-    spellScale      = spells[caster.unitclass][spellChoice][3]
-    spellCooldown   = spells[caster.unitclass][spellChoice][4]
-    spellMag = int(random.randint(spellBaseMin, spellBaseMax) + (caster.int * spellScale))
-    spellInfo = spellName, spellType, spellBaseMin, spellBaseMax, spellScale, spellMag
-    if spellType is 'dmg':              #i want this to be automated
-        defender.curhp -= spellMag
-    if spellType is 'heal':
-        caster.curhp += spellMag
-        if caster.curhp > caster.maxhp:
-            caster.curhp = caster.maxhp
-    combat.determineDeath(defender)
-    spellCooldowns[spellName] = spellCooldown
-    return spellInfo
-        
+
+
+    ###############################
+    #       abilities list        #
+    ###############################
+
+abilities = {
+    #               name                            description                  occurs         target       affects      magnitude    cooldown
+    #                                                   0                           1              2            3            4             5            
+'berserker': {
+              'Bloodlust':                       ('Heals for a small amount', 'deathblow',       'heal',     'curhp',         .2,          0,       Buff_bloodlust),
+        'Overhead Strike':           ('Strikes the target with medium force',    'active',        'dmg',    'weapon',        1.5,          3,                 None),
+    },
+
+'rogue': {
+               'Momentum': ('Next attack is guaranteed to strike critically', 'deathblow',       'dmg',      'crit',        100,          0,         Buff_momentum),
+               'Backstab':        (    'Attacks from behind, piercing armor',    'active',       'dmg',        'ac',        200,          6,         Buff_backstab),
+    },
+
+'warrior': {
+            'Shield Slam':                         ('Returns blocked damage',     'active',      'dmg', 'blockdmg',          1,          2,                  None),
+           'Shield Block':          ('Chance to block doubled for next turn',  'deathblow',     'buff', 'blockval',          2,          0,     Buff_shield_block),
+    },
+
+'wizard': {
+            'Overcharge':            ('Increases power of next spell by 40%',     'active',     'buff', 'spellmag',        .4,          4,        Buff_overcharge),
+
+    }
+}
 
     
+    ###############################
+    #     activate abilities      #
+    ###############################
+
