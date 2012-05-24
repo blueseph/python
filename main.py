7eb346877145ef30ca6890d4876291e22d5fb0a3
Iam Blueseph <blueseph@gmail.com>
2012-05-24 15:52:49 -0400

    creates and draws map on init. actually added player input

--- main.py
+++ main.py
@@ -3,18 +3,30 @@ import equipment
 import classes
 import time
 import gameturn
+import combatdisplay
+import board
+from msvcrt import getch
 
-    ###############################
-    #   chooses class and fights  #
-    ###############################
-    
-if __name__ == "__main__":
-        classes.chooseClass()
-        classes.chooseMonsterClass()
-        combat.fight()
+ 
+classes.chooseClass()
+classes.chooseMonsterClass()
+board.createMap()
+board.current_dungeon_map[1].draw()
 
-        while gameturn.playerDeath is False:
-            classes.chooseMonsterClass()        #rerolls monster
-            combat.fight()                      #fights again
-
-        time.sleep(5)
+while gameturn.playerDeath is False:
+    input = getch()
+    if 'w' in str(input): 
+        classes.creatures['player'].move(0, -1) 
+        gameturn.doGameTurn()
+    elif 's' in str(input):
+        classes.creatures['player'].move(0, 1)
+        gameturn.doGameTurn()
+    elif 'a' in str(input):
+        classes.creatures['player'].move(-1, 0)
+        gameturn.doGameTurn()
+    elif 'd' in str(input):
+        classes.creatures['player'].move(1, 0)
+        gameturn.doGameTurn()
+    elif 'xff' in str(input):
+        classes.creatures['player'].move(1, 0)
+        gameturn.doGameTurn() 
